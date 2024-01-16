c888f456afcbf3ef428fa7d19a37396942025cfa
putinpho <thienlam2000@gmail.com>
2024-01-15 17:33:10 -0800

    Update site config

--- apps/resume-template/src/main.tsx
+++ apps/resume-template/src/main.tsx
@@ -1,20 +1,20 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import "@fontsource/poppins"; // Defaults to weight 400
-import "@fontsource/poppins/400.css"; // Specify weight
-import "@fontsource/poppins/400-italic.css";
+import '@fontsource/poppins'; // Defaults to weight 400
+import '@fontsource/poppins/400.css'; // Specify weight
+import '@fontsource/poppins/400-italic.css';
 import '@fontsource/satisfy';
 
 import App from './app/app';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
